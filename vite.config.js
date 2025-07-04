ad44192d1d6b256ddde7f4eb2cee9ae8f83375cf
ErickML69 <erickml.7u7@gmail.com>
2025-07-04 09:32:32 +0000

    cambios 9

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'public/index.html')
-      }
+      input: path.resolve(__dirname, './public/index.html')
     }
   }
 })
